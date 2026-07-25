73c9cdabdb61a944d7f598e67934f0e0bcb634e6
juehara-crypto <j863281@yahoo.co.jp>
2026-07-25 15:24:07 +0900

    fix SITE_TITLE quote issue

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 ﻿// Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'juehara-crypto's Tech Blog';
+export const SITE_TITLE = "juehara-crypto's Tech Blog";
 export const SITE_DESCRIPTION = 'Ansible・インフラ・自動化をテーマにした技術ブログ';
