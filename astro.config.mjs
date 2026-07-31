33998eb97ac124b5d56396a439716ea9eff61407
juehara-crypto <j863281@yahoo.co.jp>
2026-07-31 14:41:43 +0900

    ansible-drift-04.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+import mermaid from 'astro-mermaid'; //  AstroでMermaid図を表示させる設定
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://juehara-crypto.github.io',
-	integrations: [mdx(), sitemap()],
+    integrations: [
+		    mdx(),
+		    sitemap(),
+		    mermaid(), //  AstroでMermaid図を表示させる設定
+	],
 	fonts: [
 		{
 			provider: fontProviders.local(),
